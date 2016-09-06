b9837e281aee67c4dfd2aa6a6a49f951dca49523
Simon GH <groundhog@gmx.net>
2016-09-06 17:35:24 +0200

    Devices: Add Onyx i86ML e-book reader detection and dither activation
    Recognizes i86ML as an e-book to optimize for black and white / switch
    on dithering

--- src/Android/Main.cpp
+++ src/Android/Main.cpp
@@ -2,7 +2,7 @@
 Copyright_License {
 
   XCSoar Glide Computer - http://www.xcsoar.org/
-  Copyright (C) 2000-2013 The XCSoar Project
+  Copyright (C) 2000-2016 The XCSoar Project
   A detailed list of copyright holders can be found in the file "AUTHORS".
 
   This program is free software; you can redistribute it and/or
@@ -144,7 +144,10 @@ Java_org_xcsoar_NativeView_initializeNative(JNIEnv *env, jobject obj,
 #ifdef __arm__
   is_nook = strcmp(native_view->GetProduct(), "NOOK") == 0;
   is_onyx_ebook = strcmp(native_view->GetProduct(), "T68Lynx") == 0
-               || strcmp(native_view->GetProduct(), "T68") == 0;
+               || strcmp(native_view->GetProduct(), "T68") == 0
+               || strcmp(native_view->GetProduct(), "T68ML") == 0
+               || strcmp(native_view->GetProduct(), "I68ML") == 0
+               || strcmp(native_view->GetProduct(), "I68MLPlus") == 0;
 #endif
 
   event_queue = new EventQueue();
